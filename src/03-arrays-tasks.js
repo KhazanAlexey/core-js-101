141e9a48cecdee32129a7dec4bf80ea3f1401493
Alexey <64707536+KhazanAlexey@users.noreply.github.com>
2022-07-24 01:18:25 +0300

    feat: 03-arrays-tasks.js

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -297,8 +297,9 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  return arr.reverse()
+    .slice(0, 3);
 }
 
 
@@ -315,8 +316,8 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((item) => (typeof item === 'number' ? item > 0 : false)).length;
 }
 
 /**
@@ -332,8 +333,9 @@ function getPositivesCount(/* arr */) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const map = new Map([['zero', 0], ['one', 1], ['two', 2], ['three', 3], ['four', 4], ['five', 5], ['six', 6], ['seven', 7], ['eight', 8], ['nine', 9]]);
+  return arr.sort((a, b) => map.get(a) - map.get(b));
 }
 
 /**
